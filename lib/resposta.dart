d5bbcb8ca396269e1d4f195ddfcaf2d94cf7f5ef
Neuber de Castro <xneuber@gmail.com>
2022-03-21 22:41:36 -0300

    Flutter aula 56

---
+++ lib/resposta.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Resposta extends StatelessWidget {
+  final String texto;
+  final void Function() onSelecao;
+
+  Resposta(this.texto, this.onSelecao);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: RaisedButton(
+        textColor: Colors.white,
+        color: Colors.blue,
+        child: Text(texto),
+        onPressed: onSelecao,
+      ),
+    );
+  }
+}
